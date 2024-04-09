fbd9e5829f85afa24449c452cd34c93456515056
Zero-atom <Atom.vip.gold@gmail.com>
2024-04-09 13:57:15 +0300

    project start

---
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -0,0 +1,20 @@
+package com.example.tacocloud.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+
+}
